4e8de0c6bb1003192f288136f4660ee7fba063e5
ZuRun's MacBook Pro <hi@zuhr.me>
2018-10-01 23:51:04 +0800

    排查 使用zookeeper时,如果消费方使用了拦截器,会导致@Reference注解为null的原因

--- tracing-dubbo/src/main/java/cn/zull/tracing/dubbo/filter/DubboFilter.java
+++ tracing-dubbo/src/main/java/cn/zull/tracing/dubbo/filter/DubboFilter.java
@@ -1,5 +1,6 @@
-package cn.zull.tracing.dubbo.product.filter;
+package cn.zull.tracing.dubbo.filter;
 
+import cn.zull.tracing.core.TraceContext;
 import cn.zull.tracing.core.utils.SpringApplicationContext;
 import cn.zull.tracing.dubbo.DubboTraceContext;
 import cn.zull.tracing.dubbo.RpcTraceContext;
@@ -8,30 +9,39 @@ import com.alibaba.dubbo.common.extension.Activate;
 import com.alibaba.dubbo.rpc.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * @author zurun
- * @date 2018/9/17 10:38:28
+ * @date 2018/9/27 22:34:42
  */
-@Activate(group = Constants.PROVIDER)
+@Activate(group = {Constants.PROVIDER, Constants.CONSUMER})
 public class DubboFilter implements Filter {
-
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
-    RpcTraceContext traceContext = SpringApplicationContext.getBean(DubboTraceContext.class);
+    @Autowired
+    RpcTraceContext traceContext;
 
     public DubboFilter() {
-        System.out.println("-------DubboFilter-------");
+        System.out.println("-------Dubbo Filter-------");
     }
 
     @Override
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
         // 线程入口是dubbo的,记得先清空线程绑定的变量(使用线程池的情况下)
-        logger.info("filter");
+        logger.info("dubbo filter");
+        String sideVal = invoker.getUrl().getParameter(Constants.SIDE_KEY);
+        if (Constants.CONSUMER_SIDE.equals(sideVal)) {
+            traceContext.consumer(traceDTO -> {
 
-        traceContext.consumer(traceDTO -> {
-            logger.info("traceDTO:{}", traceDTO);
-        });
+            });
+        } else {
+            traceContext.product();
+        }
         return invoker.invoke(invocation);
+    }
+
+    private TraceContext getTraceContext(){
+        return  SpringApplicationContext.getBean(DubboTraceContext.class);
     }
 }
