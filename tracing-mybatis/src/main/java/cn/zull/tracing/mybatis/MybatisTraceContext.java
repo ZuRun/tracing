984410a07b5dd1c4b5211a2d349c28a8b1532340
ZuRun's MacBook Pro <hi@zuhr.me>
2018-10-18 23:57:43 +0800

    mybatis

---
+++ tracing-mybatis/src/main/java/cn/zull/tracing/mybatis/MybatisTraceContext.java
@@ -0,0 +1,9 @@
+package cn.zull.tracing.mybatis;
+
+/**
+ * @author zurun
+ * @date 2018/10/18 23:55:11
+ */
+public interface MybatisTraceContext {
+
+}
---
+++ tracing-mybatis/src/main/java/cn/zull/tracing/mybatis/TracingInterceptor.java
@@ -0,0 +1,8 @@
+package cn.zull.tracing.mybatis;
+
+/**
+ * @author zurun
+ * @date 2018/10/18 23:55:45
+ */
+public class TracingInterceptor implements Interceptor  {
+}
