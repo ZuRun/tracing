ce30e15ed39cdada74fb141b4fd331c53b0f92a5
ZuRun's MacBook Pro <hi@zuhr.me>
2018-10-31 01:29:38 +0800

    0.0.10-RELEASE

--- mvn-repository.md
+++ mvn-repository.md
@@ -8,7 +8,7 @@
 1. 把父模块更新到指定版本号，然后更新子模块，与父模块有相同的版本号
 
     ```
-    mvn versions:set -DnewVersion=0.0.9-RELEASE
+    mvn versions:set -DnewVersion=0.0.10-RELEASE
     mvn -N versions:update-child-modules  
     ```
 
--- tracing-core/src/main/java/cn/zull/tracing/core/configuration/MyBeanDefinitionRegistryPostProcessor.java
+++ tracing-core/src/main/java/cn/zull/tracing/core/configuration/MyBeanDefinitionRegistryPostProcessor.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
 import org.springframework.beans.factory.support.RootBeanDefinition;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.stereotype.Component;
 
 /**
  * 实现自定义的注册bean定义的逻辑
@@ -14,8 +16,8 @@ import org.springframework.beans.factory.support.RootBeanDefinition;
  * @author zurun
  * @date 2018/10/15 20:59:59
  */
-//@Component
-//@DependsOn("springApplicationContextUtils")
+@Component
+@DependsOn("springApplicationContextUtils")
 public class MyBeanDefinitionRegistryPostProcessor implements BeanDefinitionRegistryPostProcessor {
     @Autowired
     TracingProperties tracingProperties;
